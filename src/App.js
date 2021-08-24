e41fe9944ed7cbacc805e80654c0cf31166df459
Padraig Lamont <padraiglamont@gmail.com>
2021-08-24 10:47:17 +0800

    Start to setup basic React + Firebase + Tailwind project using create-react-app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+/*
+ * This file has nothing to do with the misinformation game,
+ * but I've left it in here as an example. -Paddy L.
+ */
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+/*
+ * This file has nothing to do with the misinformation game,
+ * but I've left it in here as an example. -Paddy L.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import firebase from "./utils/firebase";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
