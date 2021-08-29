0a95de54f19b035c1efae5538a2106fab51bb361
dayman <danny.marwick@yahoo.com>
2021-08-29 18:35:49 +0800

    rough identification screen

--- src/screens/GameIdentification.js
+++ src/screens/GameIdentification.js
@@ -6,21 +6,10 @@ import "../App.css"
 
 function GameIdentification() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/screens/GameIdentification.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div className="border-2 border-black p-4 grid space-y-2 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <p className="font-bold text-center -mt-5 p-4">Enter your access ID</p>
+            <input className="border border-black rounded-md justify-self-center mx-4 bg-purple-200" placeholder="Identification Number"></input>
+            <button className="py-1 mx-4 border border-black rounded-md text-white bg-blue-500">Login</button>
         </div>
     );
 }
