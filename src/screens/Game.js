e62ea7a57990a803c0c038bf3f5c2cc0808b0bf5
Acoding21 <83854174+Acoding21@users.noreply.github.com>
2021-09-02 09:04:28 +0800

    Update Game.js

--- src/screens/Game.js
+++ src/screens/Game.js
@@ -1,26 +1,46 @@
 import logo from "../logo.svg";
-
 // Please use Tailwind CSS (https://tailwindcss.com/)
 // instead of plain CSS where possible.
 import "../App.css"
 
 function Game() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/screens/Game.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+        <div class="h-screen w-6/12 p-0 flex flex-row flex-col text-center content-center place-content-center space-y-0 fixed left-1/2 transform -translate-x-1/2">
+            <header class="h-3/4 p-0 flex flex-col border-2 border-black grid space-y-0">
+                <header class="flex flex-row flex-wrap text-center content-center place-content-center space-y-0 gap-0 border-2 border-grey">
+                    <h1 class="text-xl">Source 1 (</h1><h1 class="text-green-700 text-xl">87</h1><h1 class="text-xl">) </h1>
+                    <p class="w-full"> <br/>999 Followers</p>
+                </header>
+                <h1 class="flex flex-col text-center content-center place-content-center space-y-0">
+                    Image Post
+                    <img src={logo} className="App-logo" alt="logo" />
+                </h1>
+                <header class="flex flex-row flex-wrap text-center content-center place-content-center space-y-0 border-2 border-grey">
+                    <h1 class="text-xl">User 1 (</h1><h1 class="text-red-500 text-xl">10</h1><h1 class="text-xl">) </h1>
+                    <p class="w-full"> <br/>This content is obviously fake</p>
+                </header>
+                <header class="flex flex-row flex-wrap text-center content-center place-content-center space-y-0 border-2 border-grey">
+                    <h1 class="text-xl">User 2 (</h1><h1 class="text-yellow-500 text-xl">50</h1><h1 class="text-xl">) </h1>
+                    <p class="w-full"> <br/>I like this content</p>
+                </header>
             </header>
+            <footer className="h-1/4">
+                <footer className="h-1/2 p-4 border-2 border-black">
+                    <button class="px-2"> Like </button>
+                    <button class="px-2"> Dislike </button>
+                    <button class="px-2"> Block </button>
+                    <button class="px-2"> Repost </button>
+                    <button class="px-2"> Flag </button>
+                </footer>            
+                <footer className="h-1/2 p-4 border-2 border-black">
+                    <p>
+                       Your Progress
+                    </p>
+                    <p>
+                        Followers:999 Credability: 000
+                    </p>
+                </footer>
+            </footer>
         </div>
     );
 }
