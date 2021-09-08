7f636a58e0b9486e6ef3f0c701e62158f676d1b2
Padraig Lamont <padraiglamont@gmail.com>
2021-09-08 11:28:03 +0800

    Disable URL Upload, add help for uploading XLSX files

--- src/components/StudyUpload.js
+++ src/components/StudyUpload.js
@@ -2,8 +2,8 @@ import "../index.css"
 import {Component} from "react";
 import {readStudyWorkbook} from "../model/studyWorkbookReader";
 import StatusLabel, {Status} from "./StatusLabel";
-import {getChangesToAndFromJSON, Post, Source, Study} from "../model/study";
-import {odiff} from "../utils/odiff";
+import {getChangesToAndFromJSON} from "../model/study";
+import xlsxHelp from "./help-export-to-xlsx.png"
 
 const Excel = require('exceljs');
 
@@ -97,7 +97,7 @@ export class StudyUploadForm extends Component {
         reader.onload = (event) => {
             this.readXLSX(event.target.result, (status) => this.updateState({ fileStatus: status }));
         };
-        reader.onerror = (event) => {
+        reader.onerror = () => {
             this.updateState({
                 fileStatus: Status.error([
                     <strong>Error reading file:</strong>,
@@ -129,7 +129,7 @@ export class StudyUploadForm extends Component {
         const request = new XMLHttpRequest();
         request.open("GET", xlsxURL, true);
         request.responseType = "arraybuffer";
-        request.onreadystatechange = (event) => {
+        request.onreadystatechange = () => {
             if (request.readyState < 4)
                 return;
 
@@ -140,7 +140,11 @@ export class StudyUploadForm extends Component {
                 this.updateState({
                     urlStatus: Status.error([
                         <strong>Unable to download spreadsheet{request.statusText && ": "}</strong>,
-                        request.statusText + " (Status " + request.status + ")"
+                        request.statusText + " (Status " + request.status + ")",
+                        (request.statusText === "Unauthorized" && request.status === 401 ?
+                            " If your Google Sheets spreadsheet is private, " +
+                                "you will need to upload it as an XLSX file." :
+                            null)
                     ])
                 });
             }
@@ -160,29 +164,15 @@ export class StudyUploadForm extends Component {
 
     render() {
         return (
-            <form className="flex flex-col items-start" onSubmit={evt => this.handleURLSubmit(evt)}>
-                <span className="flex my-2">
-                    Enter your Google Sheets spreadsheet URL or ID:
+            <form className="flex flex-col items-start max-w-xl"
+                  onSubmit={evt => this.handleURLSubmit(evt)}>
+
+                {/*<h3 className="flex mt-4 mb-1 font-bold text-lg">*/}
+                {/*    Option 1. Upload XSLX File*/}
+                {/*</h3>*/}
+                <span className="flex mt-4 mb-3">
+                    Upload your study configuration spreadsheet as an XLSX file:
                 </span>
-
-                <div className="flex flex-1 w-full">
-                    <input type="text" className="max-w-lg outline-none
-                            flex-1 p-2 bg-gray-100 border border-blue-500
-                            border-r-0 rounded-l-lg rounded-r-none"
-                        onChange={evt => this.onURLChange(evt)} />
-                    <input type="submit" className="
-                            mr-2 px-4 py-2 text-blue-500 rounded bg-white
-                            cursor-pointer border border-blue-500
-                            rounded-r-lg rounded-l-none flex-grow-0
-                            hover:bg-blue-500 hover:text-white"
-                        value="Fetch" />
-                </div>
-                <StatusLabel status={this.state.urlStatus} className="my-2" />
-
-                <span className="flex mb-2 mt-6">
-                    Or, upload your study configuration spreadsheet as an XLSX file:
-                </span>
-
                 <div className="flex flex-1 w-full">
                     <label className="flex flex-row justify-items-center py-2 px-3 bg-white
                                       text-blue-500 rounded-lg
@@ -195,6 +185,47 @@ export class StudyUploadForm extends Component {
                     </label>
                 </div>
                 <StatusLabel status={this.state.fileStatus} className="my-2" />
+                <span className="pt-4">
+                    You can download your Google Sheets spreadsheet as an XLSX
+                    file by selecting <strong>File</strong>
+                    &#10147; <strong>Download</strong>
+                    &#10147; <strong>Microsoft Excel (.xlsx) </strong>
+                    within your configuration spreadsheet.
+                </span>
+                <img src={xlsxHelp} className="max-h-64 mt-2"
+                     alt="Steps to download spreadsheet as XLSX file" />
+
+                {/* URL Upload is currently disabled due to permissions issues. */}
+                {/*<h3 className="flex mt-6 mb-1 font-bold text-lg">*/}
+                {/*    Option 2. Upload Directly from URL*/}
+                {/*</h3>*/}
+                {/*<span className="flex mb-3">*/}
+                {/*    You can also enter your public Google Sheets spreadsheet URL or ID:*/}
+                {/*</span>*/}
+                {/*<div className="flex flex-1 w-full">*/}
+                {/*    <input type="text" className="max-w-lg outline-none*/}
+                {/*            flex-1 p-2 bg-gray-100 border border-blue-500*/}
+                {/*            border-r-0 rounded-l-lg rounded-r-none"*/}
+                {/*        onChange={evt => this.onURLChange(evt)} />*/}
+                {/*    <input type="submit" className="*/}
+                {/*            mr-2 px-4 py-2 text-blue-500 rounded bg-white*/}
+                {/*            cursor-pointer border border-blue-500*/}
+                {/*            rounded-r-lg rounded-l-none flex-grow-0*/}
+                {/*            hover:bg-blue-500 hover:text-white"*/}
+                {/*        value="Fetch" />*/}
+                {/*</div>*/}
+                {/*<StatusLabel status={this.state.urlStatus} className="my-2" />*/}
+                {/*<span className="pt-2">*/}
+                {/*    This requires that you make your configuration spreadsheet*/}
+                {/*    readable by anyone. Therefore, uploading the study as an*/}
+                {/*    XLSX file should be preferred if you wish to keep your*/}
+                {/*    study configuration private. If you wish to convert your*/}
+                {/*    spreadsheet to public, click on <strong>Share </strong>*/}
+                {/*    and then click on <strong>Change to anyone with the link </strong>*/}
+                {/*    underneath <strong>Get link </strong>. This will allow anyone*/}
+                {/*    to read your study configuration if they stumble upon the link*/}
+                {/*    to it, but they won't be able to edit the spreadsheet.*/}
+                {/*</span>*/}
             </form>
         );
     }
@@ -208,17 +239,15 @@ export default class StudyUpload extends Component {
                 <div className="fixed left-0 top-0 w-full h-full z-40 bg-black opacity-20" />
 
                 {/* Place study upload form above grayed-out background. */}
-                <div className="fixed left-0 top-0 w-full h-full z-50
-                        flex flex-col content-center">
-                    <div className="flex-initial flex flex-col
-                            m-8 p-8 shadow rounded-md bg-white divide-y">
+                <div className="fixed left-0 top-0 w-full h-full overflow-y-scroll z-50
+                        flex flex-row justify-center items-start">
+                    <div className="flex flex-col m-8 p-8 shadow
+                            rounded-md bg-white divide-y max-w-xl">
                         <h2 className="py-2 text-4xl">Upload Study</h2>
-                        <div className="pt-6">
-                            <StudyUploadForm onStudyLoad={(study) => {
-                                console.log(study);
-                                window.lastStudy = study;
-                            }} />
-                        </div>
+                        <StudyUploadForm onStudyLoad={(study) => {
+                            console.log(study);
+                            window.lastStudy = study;
+                        }} />
                     </div>
                 </div>
             </div>
