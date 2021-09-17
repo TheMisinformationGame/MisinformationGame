364026e00c0cf202039d610089484efefd88aff3
YUNHUI RAO <50687613+AlanRYH@users.noreply.github.com>
2021-09-17 15:02:40 +0800

    Update AdminHome.js

--- src/screens/AdminHome.js
+++ src/screens/AdminHome.js
@@ -1,28 +1,71 @@
-import logo from "../logo.svg";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
 
-// Please use Tailwind CSS (https://tailwindcss.com/)
-// instead of plain CSS where possible.
-import "../App.css"
+class AdminPage extends React.Component{
+    render() {
+        return(Admin())
+    }
+};
 
-function AdminHome() {
+function Admin() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/screens/AdminHome.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+    <div style={{backgroundImage: 'url(https://www.toptal.com/designers/subtlepatterns/patterns/papyrus.png)'}}>
+        {/**mx-auto means put the container in the middle */}
+    <div className="container min-h-screen w-3/5 mx-auto bg-blue-300 bg-opacity-10 pb-10" >
+        {/*The navigation bar */}
+        <table className="h-10 max-h-20 min-h-10 border-collapse border-black border-2 border-solid pt-3 pb-3 pl-3 w-full bg-gray-100">
+        <tr>
+            <td className="w-9/10">
+                <h2 className="font-black text-xl">The Misinformation Game</h2>
+            </td>
+        
+            {/*Gotta add an icon here and the link to the upload page*/}
+            <a href="https://google.com">
+                <button className="h-full bg-blue-400 w-1/10 float-right text-white text-center border-black border-l-2 border-solid">Upload Study</button>
+            </a>
+        </tr>
+        </table>
+    
+    <div className="box-border h-5/6 w-full pt-10 px-20">
+        <div className="grid grid-cols-2 gap-7">
+            <div className="rounded-xl border-2 border-gray-400 p-5">
+                <a href="https://google.com" className="text-blue-600 text-lg font-bold">First Study</a>
+                <p><b>Author: </b>Assoc/Prof<br/></p>
+                <p><b>Participants: </b>98<br/></p>
+            </div>
+
+            <div className="rounded-xl border-2 border-gray-400 p-5">
+                <a href="https://google.com" className="text-blue-600 text-lg font-bold">Second Study</a>
+                <p><b>Author: </b>Ben/Prof<br/></p>
+                <p><b>Participants: </b>56<br/></p>
+            </div>
+
+            <div className="rounded-xl border-2 border-gray-400 p-5">
+                <a href="https://google.com" className="text-blue-600 text-lg font-bold">Third Study</a>
+                <p><b>Author: </b>Chist/Prof<br/></p>
+                <p><b>Participants: </b>78<br/></p>
+            </div>
+
+            <div className="rounded-xl border-2 border-gray-400 p-5">
+                <a href="https://google.com" className="text-blue-600 text-lg font-bold">Forth Study</a>
+                <p><b>Author: </b>Danny/Prof<br/></p>
+                <p><b>Participants: </b>98<br/></p>
+            </div>
+
+            <div className="rounded-xl border-2 border-gray-400 p-5">
+                <a href="https://google.com" className="text-blue-600 text-lg font-bold">Fifth Study</a>
+                <p><b>Author: </b>Emma/Prof<br/></p>
+                <p><b>Participants: </b>58<br/></p>
+            </div>
+            
         </div>
+    </div>
+    
+    </div>
+    </div>
     );
 }
 
-export default AdminHome;
+ReactDOM.render(<AdminPage/>, document.getElementById('root'));
+export default AdminPage;
