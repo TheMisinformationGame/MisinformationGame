82fd5379d312b0b78a19b8ce7d729e465a6ff2b0
Padraig Lamont <padraiglamont@gmail.com>
2021-09-28 12:58:58 +0800

    Fix the shadows going missing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      width: {"30": "7.5rem"}
+      width: {
+        "30": "7.5rem"
+      },
+      boxShadow: {
+        'up': '0 10px 20px 5px rgba(0, 0, 0, 0.3)',
+      }
     },
-    boxShadow: {
-      'up': '0 10px 20px 5px rgba(0, 0, 0, 0.3)',
-    }
   },
   variants: {
     extend: {
